1eb7ff85f94681b82d9e892e572b5de2b3550aaf
Benjamin-Bemis <bbemis00@gmail.com>
2024-10-08 11:58:00 -0400

    initalizing the hw

---
+++ Homework/Assignment 3/hw_3.m
@@ -0,0 +1,22 @@
+%{
+
+@author: Benjamin Bemis Ph.D Student, 
+Advisor: Dr Juliano
+
+
+Description:
+AME 60614: Numerical Methods
+Homework: 3
+Due: 10/10/2024
+
+
+%}
+%% Preperation of the workspace
+fontsize = 16;
+
+
+% set(0,'DefaultFigureWindowStyle','docked')
+set(0,'DefaultTextInterpreter','latex')
+set(0,'DefaultAxesFontSize',fontsize)
+set(0,'DefaultLegendFontSize',fontsize)
+colors  = ["#000000","#1b9e77","#d95f02","#7570b3","#0099FF"];
